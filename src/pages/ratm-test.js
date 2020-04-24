03aad59629eae95d2fe2cda87bae7e89903f2d5a
Israel Morales <isramv@gmail.com>
2020-04-24 14:23:22 -0500

    First test

--- src/pages/ratm-test.js
+++ src/pages/ratm-test.js
@@ -1,43 +1,59 @@
 import React from "react"
-import {StaticQuery, graphql} from "gatsby"
+import { StaticQuery, graphql } from "gatsby"
+
+const BlockIsra = ({props}) => {
+    const blockData = props.originalContent;
+    let heroAttributes = {}
+
+    if (props.__typename === 'LazyblockHeroBlock') {
+        heroAttributes = props.attributes;
+    }
+    
+    return(
+        <>
+            <h1>{props.__typename}</h1>
+            <div dangerouslySetInnerHTML={{ __html: blockData}}/>
+            {/* If type hero    */}
+            <div className="Hero">
+                <h2>{heroAttributes.heroTitle}</h2>
+                {(heroAttributes.ctaUrl) ?  heroAttributes.ctaUrl : null }
+            </div>
+
+        </>
+    )
+}
+
+const ComponentTest = ({data}) => {
+    const blocks = JSON.parse(data.wpcontent.page.blocksJSON)
+    
+    return (<div>
+        <h1>Hello Friend</h1>
+        <ul>
+            {blocks.map((block, i) => {
+                return (<BlockIsra key={i} props={block}/>)
+            })}
+        </ul>
+    </div>)
+}
 
 const ComponentName = () => (
     <StaticQuery
-      query={graphql`
+    query={graphql`
       {
         __typename
         wpcontent {
-          generalSettings {
-            title
-            url
-          }
-          pages {
-            edges {
-              node {
-                title
-                uri
-                status
-                slug
-              }
-            }
+          page(id: "cGFnZToxMA==") {
+            id
+            blocksJSON
           }
         }
       }
     `}
-        render={data =>
-            <pre>
-            {/*{JSON.stringify(data, null, 4)}*/}
-                <div>
-                <h1>{data.wpcontent.generalSettings.title}</h1>
-                <span>{data.wpcontent.generalSettings.url}</span>
-                <article>
-                    <h2>{data.wpcontent.pages.edges[0].node.title}</h2>
-                    <h3>{data.wpcontent.pages.edges[0].node.slug}</h3>
-                </article>
-            </div>
-        </pre>
-        }
+    render={data =>
+        <pre>
+            <ComponentTest data={data}/>
+        </pre>}
     />
-)
+);
 
 export default ComponentName
