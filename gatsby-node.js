83adb5354e81315b23f7881be97f33099b694ab8
Israel Morales <isramv@gmail.com>
2020-05-04 09:57:48 -0500

    advancing

--- gatsby-node.js
+++ gatsby-node.js
@@ -32,14 +32,25 @@ exports.createPages = async ({ actions, graphql }) => {
 
   const pages = result.data.wpcontent.pages.nodes
   const mediaItems = result.data.wpcontent.mediaItems.nodes
-  
+
+  function attachImage() {
+    
+  }
+
+  // Returns a page index.
+  function lookForParent(id) {
+    return _.findKey(pages, { 'id': id });
+  }
+
   mediaItems.forEach(mediaItem => {
-    pages.forEach(page => {
-      if (page.id === mediaItem.parent.id) {
-        page.mediaItems = []
-        page.mediaItems.push(mediaItem)
+    let parentIndex = lookForParent(mediaItem.parent.id)
+    if (typeof parentIndex === 'string') {
+      if (typeof pages[parentIndex].mediaItems !== 'undefined') {
+        pages[parentIndex].mediaItems.push(mediaItem)
+      } else {
+        pages[parentIndex].mediaItems = []
       }
-    })
+    }
   })
 
   pages.forEach(page => {
@@ -48,8 +59,40 @@ exports.createPages = async ({ actions, graphql }) => {
       component: require.resolve('./src/templates/ratm-page-template.js'),
       context: {
         id: page.id,
-        blocks: page.blocksJSON
+        blocks: page.blocksJSON,
+        page: page
       }
     })
   })
+}
+
+exports.onCreateNode = async ({
+  node,
+  actions: { createNode },
+  store,
+  cache,
+  createNodeId,
+}) => {
+  // For all MarkdownRemark nodes that have a featured image url, call createRemoteFileNode
+  if (
+    node.internal.type === 'SitePage' && 
+    !node.isCreatedByStatefulCreatePages &&
+    _.has(node, 'context')
+  ) {
+
+    // console.log(node.context.page.mediaItems);
+
+    // let fileNode = await createRemoteFileNode({
+    //   url: node.frontmatter.featuredImgUrl, // string that points to the URL of the image
+    //   parentNodeId: node.id, // id of the parent node of the fileNode you are going to create
+    //   createNode, // helper function in gatsby-node to generate the node
+    //   createNodeId, // helper function in gatsby-node to generate the node id
+    //   cache, // Gatsby's cache
+    //   store, // Gatsby's redux store
+    // })
+    //
+    // if (fileNode) {
+    //   node.featuredImg___NODE = fileNode.id
+    // }
+  }
 }
