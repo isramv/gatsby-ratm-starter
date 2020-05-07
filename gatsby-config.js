69619ded7980c586a584167877e6d89a8ae72e7e
Israel Morales <isramv@gmail.com>
2020-05-07 14:57:15 -0500

    Added blog template

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,18 +7,7 @@ module.exports = {
   plugins: [
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-sass`,
-    {
-      resolve: "gatsby-source-graphql",
-      options: {
-        // Arbitrary name for the remote schema Query type
-        typeName: "WPGraphQL",
-        // Field under which the remote schema will be accessible. You'll use this in your Gatsby query
-        fieldName: "wpcontent",
-        // Url to query from
-        url: "https://dev-ratm.pantheonsite.io/graphql",
-        // url: "https://ratm.lndo.site/graphql",
-      },
-    },
+    `gatsby-transformer-remark`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
@@ -31,6 +20,25 @@ module.exports = {
         theme_color: `#663399`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `markdown-pages`,
+        path: `${__dirname}/src/markdown-pages`,
+      },
+    },
+    {
+      resolve: "gatsby-source-graphql",
+      options: {
+        // Arbitrary name for the remote schema Query type
+        typeName: "WPGraphQL",
+        // Field under which the remote schema will be accessible. You'll use this in your Gatsby query
+        fieldName: "wpcontent",
+        // Url to query from
+        url: "https://dev-ratm.pantheonsite.io/graphql",
+        // url: "https://ratm.lndo.site/graphql",
       },
     }
   ],
---
+++ src/templates/blog-template.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { graphql } from 'gatsby'
+
+export default function Template({data}) {
+  const { markdownRemark } = data
+  const { html } = markdownRemark
+
+  return (
+    <div dangerouslySetInnerHTML={{__html: html}}/>
+  )
+}
+
+export const pageQuery = graphql`
+  query($path: String!) {
+    markdownRemark(frontmatter: { path: { eq: $path } }) {
+      html
+      frontmatter {
+        date(formatString: "MMMM DD, YYYY")
+        path
+        title
+      }
+    }
+  }
+`
