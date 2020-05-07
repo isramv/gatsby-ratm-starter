92ec2ef83ba751c100cae64c4e7e3609130ddb5a
Israel Morales <isramv@gmail.com>
2020-05-07 12:21:08 -0500

    Updated text and image block

--- src/components/lazy/LazyblockTextAndImageBlock.js
+++ src/components/lazy/LazyblockTextAndImageBlock.js
@@ -1,30 +1,37 @@
 import React from 'react'
-import {
-  Grid
-} from 'semantic-ui-react'
+import { Grid } from 'semantic-ui-react'
 import { graphql, useStaticQuery } from 'gatsby'
 import Img from 'gatsby-image'
+import fastDecode from 'fast-decode-uri-component'
 
 export default ({block}) => {
-  // const image = JSON.parse(decodeURIComponent(block.image))
-  // const images = useStaticQuery(graphql`
-  //   {
-  //       allFile {
-  //         nodes {
-  //           name
-  //           url
-  //           childImageSharp {
-  //             fixed(width: 150) {
-  //               ...GatsbyImageSharpFixed
-  //             }
-  //           }
-  //         }
-  //       }
-  //     }
-  // `)
-  // const allImageNodes = images.allFile.nodes;
-  // const heroImage = allImageNodes.filter(imageNode => imageNode.url === image.url)
-  // const gatsbyImg = heroImage[0].childImageSharp.fixed
+  const image = JSON.parse(fastDecode(block.image))
+  const images = useStaticQuery(graphql`
+    {
+    wpcontent {
+      mediaItems {
+        nodes {
+          title
+          sourceUrl
+          imageFile {
+            childImageSharp {
+              fixed(width: 150) {
+                ...GatsbyImageSharpFixed
+              }
+            }
+          }
+        }
+      }
+    }
+  }`)
+  const allImageNodes = images.wpcontent.mediaItems.nodes
+  const heroImage = allImageNodes.filter(imageNode => {
+    return imageNode.sourceUrl === image.url
+  })
+  let gatsbyImg = null
+  if (heroImage.length > 0) {
+    gatsbyImg = heroImage[0].imageFile.childImageSharp.fixed
+  }
   return (
     <section>
       <Grid>
@@ -34,7 +41,7 @@ export default ({block}) => {
             <p dangerouslySetInnerHTML={{__html: block.copy}}/>
           </Grid.Column>
           <Grid.Column mobile={16} tablet={8} computer={8}>
-            {/*<Img fixed={gatsbyImg}/>*/}
+            <Img fixed={gatsbyImg}/>
           </Grid.Column>
         </Grid.Row>
       </Grid>
