c6da13d979631978389bacf05042f3eaeedd50f6
Israel Morales <isramv@gmail.com>
2020-05-07 21:23:03 -0500

    layout updated

--- src/components/layout/ratm-layout.js
+++ src/components/layout/ratm-layout.js
@@ -16,32 +16,32 @@ import {
 
 const Layout = ({ children }) => {
   const [visible, setVisible] = useState(false)
-  function toggleMobileNav(e) {
+
+  function toggleMobileNav (e) {
     e.preventDefault()
     setVisible(!visible)
   }
+
   return (
     <div className='pageContainer'>
-      <Responsive className='header'>
-        <Menu inverted style={{borderRadius: `0`}}>
+      <div className='header'>
+        <Menu inverted style={{ borderRadius: `0` }}>
           <Container>
             <Responsive maxWidth={760}>
-              <Container>
-                <Menu inverted>
-                  <MenuItem onClick={toggleMobileNav}>
-                    <Icon name='content'/>
-                  </MenuItem>
-                </Menu>
-              </Container>
+              <Menu inverted>
+                <MenuItem onClick={toggleMobileNav}>
+                  <Icon name='content'/>
+                </MenuItem>
+              </Menu>
             </Responsive>
             <Responsive minWidth={761}>
-              <Menu inverted pointing secondary>
+              <Menu className='main-menu--desktop' inverted pointing secondary>
                 <MenuItems/>
               </Menu>
             </Responsive>
           </Container>
         </Menu>
-      </Responsive>
+      </div>
       <SidebarPushable>
         <Sidebar as={Menu}
                  animation='overlay'
@@ -52,7 +52,7 @@ const Layout = ({ children }) => {
                  visible={visible}
                  width='thin'
         >
-          <MenuItem onClick={toggleMobileNav} >
+          <MenuItem onClick={toggleMobileNav}>
             <Icon name='close' circular={true} size='small'/>
           </MenuItem>
           <MenuItems/>
